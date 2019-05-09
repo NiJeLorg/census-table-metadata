16e791fa6a1a2d6461eadf6a31f925383ca030e2
JD Godchaux <jd@nijel.org>
2019-05-09 16:36:15 -0500

    update metadata load script

--- census_metadata_load.sql
+++ census_metadata_load.sql
@@ -1,79 +1,79 @@
-COPY public.census_tabulation_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/unified_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY public.census_tabulation_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/unified_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2007_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2007_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2007_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2007_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2007_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2007_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2007_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2007_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2007_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2007_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2007_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2007_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2007_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2007_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2007_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2007_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2008_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2008_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2008_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2008_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2008_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2008_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2008_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2008_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2008_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2008_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2008_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2008_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2008_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2008_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2008_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2008_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2009_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2009_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2009_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2009_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2009_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2009_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2009_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2009_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2009_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2009_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2009_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2009_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2009_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2009_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2009_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2009_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2010_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2010_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2010_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2010_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2010_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2010_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2010_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2010_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2010_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2011_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2011_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2011_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2011_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2011_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2011_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2011_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2011_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2011_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2012_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2012_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2012_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2012_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2012_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2012_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2012_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2012_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2012_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2013_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2013_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2013_3yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2013_3yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_3yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_3yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_3yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_3yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2013_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2013_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2013_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2013_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2013_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2014_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2014_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2014_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2014_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2014_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2014_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2014_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2014_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2014_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2014_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2014_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2014_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2014_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2014_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2014_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2014_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2015_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2015_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2015_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2015_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2015_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2015_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2015_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2015_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2015_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2015_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2015_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2015_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2015_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2015_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2015_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2015_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2016_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2016_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2016_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2016_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2016_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2016_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2016_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2016_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2016_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2016_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2016_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2016_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2016_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2016_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2016_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2016_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2017_1yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2017_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2017_1yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2017_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2017_1yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2017_1yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2017_1yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2017_1yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
 
-COPY acs2017_5yr.census_table_metadata  FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2017_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
-COPY acs2017_5yr.census_column_metadata FROM '/home/django/sd-child/census-table-metadata/precomputed/acs2017_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2017_5yr.census_table_metadata  FROM '/home/sdcapi/census-table-metadata/precomputed/acs2017_5yr/census_table_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
+COPY acs2017_5yr.census_column_metadata FROM '/home/sdcapi/census-table-metadata/precomputed/acs2017_5yr/census_column_metadata.csv' WITH csv ENCODING 'utf8' HEADER;
